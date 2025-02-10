e59ed527d56e742896fa9a773c22c6ab39140455
mathias885 <mathias.bruni04@gmail.com>
2025-02-10 10:50:15 +0100

    fixed partecipation

--- routes/partecipation.route.js
+++ routes/partecipation.route.js
@@ -6,13 +6,13 @@ const authenticateJWT = require('../middlewares/authenticateJWT');
 
 
 // Crea una nuova partecipazione
-router.post('/',authenticateJWT, (req, res) => {
+router.post('/join',authenticateJWT, (req, res) => {
 try{
     // Istanzia una nuova partecipazione con i dati ricevuti
     const eventInstance = new partecipation({
 
         userID: req.user.userId,
-        eventID: req.body.event,
+        eventID: req.query.event,
         
     });
 
@@ -35,10 +35,10 @@ try{
 
 
 // Elimina una partecipazione specifica
-router.delete('/single',authenticateJWT, async (req, res) => {
+router.delete('/leaave',authenticateJWT, async (req, res) => {
     try {
         userID = req.user.userId;
-        eventID = req.body.event;
+        eventID = req.query.event;
 
         // Elimina la partecipazione specifica
         const result = await partecipation.deleteOne({ userID, eventID });
